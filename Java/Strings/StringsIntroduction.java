01726fcea72f875df5d082019232113715a713a5
Bharathi Vatsavai <bharathi.vats14@gmail.com>
2018-01-25 14:57:01 -0800

    Update StringsIntroduction.java

--- Java/Strings/StringsIntroduction.java
+++ Java/Strings/StringsIntroduction.java
@@ -49,7 +49,7 @@ When you capitalize the first letter of both  and  and then print them separated
 import java.io.*;
 import java.util.*;
 
-public class Solution {
+public class StringsIntroduction {
 
     public static void main(String[] args) {
         
